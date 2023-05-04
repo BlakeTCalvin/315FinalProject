334e16a11c4fb354b30f558017a1a370e6b9068a
Blake Calvin <blakecalvin@Neils-MacBook.home>
2023-05-03 18:06:33 -0700

    Dataframes added, recommended column added, matrices added.

---
+++ file_processor.py
@@ -0,0 +1,87 @@
+import pandas as pd
+
+print("------------------------------------")
+print("FILE PROCESSOR START")
+print("------------------------------------")
+
+allVideoGameData = pd.read_csv("Data/Video_Games_Sales_as_at_22_Dec_2016.csv")
+allVideoGameData = allVideoGameData.loc[:, ['Name', 'Platform', 'Genre', 'Publisher', 'Global_Sales', 'Critic_Score', 'User_Score', 'Rating']]
+
+# saving data based off of consoles
+PCData = allVideoGameData[allVideoGameData['Platform'] == 'PC']
+XOneData = allVideoGameData[allVideoGameData['Platform'] == 'XOne']
+PS4Data = allVideoGameData[allVideoGameData['Platform'] == 'PS4']
+WiiUData = allVideoGameData[allVideoGameData['Platform'] == 'WiiU']
+DSData = allVideoGameData[allVideoGameData['Platform'] == '3DS']
+N64Data = allVideoGameData[allVideoGameData['Platform'] == 'N64']
+
+# checking that dataframes were made correctly
+# Checking complete CSV was imported
+if allVideoGameData.notnull().any().any():
+    print("Complete CSV File import successful.")
+else:
+    raise AssertionError("Complete CSV File did not import correctly.")
+
+# Checking PCData was created correctly and dropping duplicates if exists
+if PCData.notnull().any().any():
+    if (PCData.duplicated(subset = ['Name']).any()):
+        PCData = PCData.drop_duplicates()
+        print("PCData creation successful.")
+else:
+    raise AssertionError("PCData creation unsuccessful.")
+
+# Checking XOneData was created correctly and dropping duplicates if exists
+if XOneData.notnull().any().any():
+    if (XOneData.duplicated(subset = ['Name']).any()):
+        XOneData = XOneData.drop_duplicates()
+    print("XOneData creation successful.")
+else:
+    raise AssertionError("XOneData creation unsuccessful.")
+
+# Checking PS4Data was created correctly and dropping duplicates if exists
+if PS4Data.notnull().any().any():
+    if (PS4Data.duplicated(subset = ['Name']).any()):
+        PS4Data = PS4Data.drop_duplicates()
+    print("PS4Data creation successful.")
+else:
+    raise AssertionError("PS4Data creation unsuccessful.")
+
+# Checking WiiUData was created correctly and dropping duplicates if exists
+if WiiUData.notnull().any().any():
+    if (WiiUData.duplicated(subset = ['Name']).any()):
+        WiiUData = WiiUData.drop_duplicates()
+    print("WiiUData creation successful.")
+else:
+    raise AssertionError("WiiUData creation unsuccessful.")
+
+# Checking DSData was created correctly and dropping duplicates if exists
+if DSData.notnull().any().any():
+    if (DSData.duplicated(subset = ['Name']).any()):
+        DSData = DSData.drop_duplicates()
+    print("3DSData creation successful.")
+else:
+    raise AssertionError("3DSData creation unsuccessful.")
+
+# Checking N64Data was created correctly and dropping duplicates if exists
+if N64Data.notnull().any().any():
+    if (N64Data.duplicated(subset = ['Name']).any()):
+        N64Data = N64Data.drop_duplicates()
+    print("N64Data creation successful.")
+else:
+    raise AssertionError("N64Data creation unsuccessful.")
+
+# if here, all made it
+print("------------------------------------")
+print("All dataframes ready.")
+print("------------------------------------")
+
+# used for finding all unique platform types and choosing which ones we want to work with
+def numberOfUniquePlatforms(dataframe):
+    platforms = dataframe['Platform'].unique()
+    print("All Platforms Evident in Dataset:")
+    for platform in platforms:
+        print(platform)
+
+print("------------------------------------")
+print("FILE PROCESSOR END")
+print("------------------------------------")
---
+++ main.py
@@ -0,0 +1,79 @@
+import warnings
+import file_processor as fp
+import matplotlib as plt
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+warnings.filterwarnings("ignore")
+
+print("------------------------------------")
+print("PROGRAM START")
+print("------------------------------------")
+
+# Fixes the labeling of the name in the csv to be parseable and readable for the matrix
+def FixTitleFormat(value):
+    return value.replace(' ', '')
+
+# fixes the recommended column to be parseable and readable for the matrix
+def ChangeFormat(value):
+    return ' '.join(value)
+
+#file_processor.numberOfUniquePlatforms(file_processor.allVideoGameData)
+# fp.PCData fp.XOneData fp.PS4Data fp.WiiUData fp.DSData fp.N64Data
+
+print("CREATING MATRICES")
+print("------------------------------------")
+
+# saving a new column in the dataframe to give our matrices values to run off of.
+# New column is just all the columns we already parsed since we deem them necessary for recommendations
+fp.PCData['Recommended'] = fp.PCData.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+fp.XOneData['Recommended'] = fp.XOneData.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+fp.PS4Data['Recommended'] = fp.PS4Data.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+fp.WiiUData['Recommended'] = fp.WiiUData.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+fp.DSData['Recommended'] = fp.DSData.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+fp.N64Data['Recommended'] = fp.N64Data.apply(lambda row: row.index[row != row['Name']].to_list(), axis = 1)
+
+# creating the transform matrix's for each platform
+# PC
+fp.PCData['Recommended'] = fp.PCData['Recommended'].apply(ChangeFormat)
+fp.PCData['Recommended'] = fp.PCData['Recommended'] + ' ' + fp.PCData['Name'].apply(ChangeFormat)
+PCMatrix = CountVectorizer().fit_transform(fp.PCData['Recommended'])
+PCcsMatrix = cosine_similarity(PCMatrix, PCMatrix)
+
+# Xbox One
+fp.XOneData['Recommended'] = fp.XOneData['Recommended'].apply(ChangeFormat)
+fp.XOneData['Recommended'] = fp.XOneData['Recommended'] + ' ' + fp.XOneData['Name'].apply(ChangeFormat)
+XOneMatrix = CountVectorizer().fit_transform(fp.XOneData['Recommended'])
+XOnecsMatrix = cosine_similarity(XOneMatrix, XOneMatrix)
+
+# PS 4
+fp.PS4Data['Recommended'] = fp.PS4Data['Recommended'].apply(ChangeFormat)
+fp.PS4Data['Recommended'] = fp.PS4Data['Recommended'] + ' ' + fp.PS4Data['Name'].apply(ChangeFormat)
+PS4Matrix = CountVectorizer().fit_transform(fp.PS4Data['Recommended'])
+PS4csMatrix = cosine_similarity(PS4Matrix, PS4Matrix)
+
+# Wii U
+fp.WiiUData['Recommended'] = fp.WiiUData['Recommended'].apply(ChangeFormat)
+fp.WiiUData['Recommended'] = fp.WiiUData['Recommended'] + ' ' + fp.WiiUData['Name'].apply(ChangeFormat)
+WiiUMatrix = CountVectorizer().fit_transform(fp.WiiUData['Recommended'])
+WiiUcsMatrix = cosine_similarity(WiiUMatrix, WiiUMatrix)
+
+# 3DS
+fp.DSData['Recommended'] = fp.DSData['Recommended'].apply(ChangeFormat)
+fp.DSData['Recommended'] = fp.DSData['Recommended'] + ' ' + fp.DSData['Name'].apply(ChangeFormat)
+DSMatrix = CountVectorizer().fit_transform(fp.DSData['Recommended'])
+DScsMatrix = cosine_similarity(DSMatrix, DSMatrix)
+
+# N64
+fp.N64Data['Recommended'] = fp.N64Data['Recommended'].apply(ChangeFormat)
+fp.N64Data['Recommended'] = fp.N64Data['Recommended'] + ' ' + fp.N64Data['Name'].apply(ChangeFormat)
+N64Matrix = CountVectorizer().fit_transform(fp.N64Data['Recommended'])
+N64csMatrix = cosine_similarity(N64Matrix, N64Matrix)
+
+print("MATRICES CREATED")
+print("------------------------------------")
+
+print("PROGRAM END")
+print("------------------------------------")
+
+print("OUTPUT")
